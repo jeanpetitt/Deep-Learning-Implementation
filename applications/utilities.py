00dcc31836de49f264d7c8961ad24ded1c14bf73
jeanpetitt <jeanpetityvelos@gmail.com>
2023-08-22 21:13:25 +0100

    update

---
+++ applications/utilities.py
@@ -0,0 +1,14 @@
+import h5py
+import numpy as np
+
+
+def load_data():
+    train_dataset = h5py.File('datasets/trainset.hdf5', "r")
+    X_train = np.array(train_dataset["X_train"][:]) # train set features
+    y_train = np.array(train_dataset["Y_train"][:]) # train set labels
+
+    test_dataset = h5py.File('datasets/testset.hdf5', "r")
+    X_test = np.array(test_dataset["X_test"][:]) # test set features
+    y_test = np.array(test_dataset["Y_test"][:]) # test set labels
+    
+    return X_train, y_train, X_test, y_test
